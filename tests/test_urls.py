4aa305e826feba70b3ccf1552a47750db35a742b
winoo19 <danielsanchezs1903@gmail.com>
2024-05-04 20:58:05 +0200

    [tests] Created tests for views and models

---
+++ tests/test_urls.py
@@ -0,0 +1,13 @@
+from django.test import SimpleTestCase
+from django.urls import reverse, resolve
+
+
+class TestUrls(SimpleTestCase):
+    def test_home_url_resolves(self):
+        pass
+
+    def test_about_url_resolves(self):
+        pass
+
+    def test_contact_url_resolves(self):
+        pass
